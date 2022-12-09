55980beb0b105d640e34895906cee65a1f314b75
ArashAzma <arashazma3@gmail.com>
2022-12-09 12:03:05 +0330

    version1

--- board.c
+++ board.c
@@ -4,9 +4,35 @@ int main()
 {
     int n;
     printf("please enter the lenght: ");
-    scanf("%d", &n);
+    scanf("%d", &n);    
+    int BOARD[n+1][n+1];
+    //SEFR KARDANE KHANE HA
+    for(int i=0; i<n+1; i++)
+    {
+        for(int j=0; j<n+1; j++)
+        {
+            BOARD[i][j] = 0;
+        }
+    }
+    //VARED KARDANE SHOMARE OFOGHI
+    for(int j=0; j<n+1; j++)
+    {
+        BOARD[0][j] = j+1;
+    } 
+    //VARED KARDANE SHOMARE AMOODI  
+    for(int i=0; i<n+1; i++)
+    {
+        BOARD[i][0] = i+1;
+    }
 
-
+    for(int i=0; i<n+1; i++)
+    {
+        for(int j=0; j<n+1; j++)
+        {
+            printf("%d ", BOARD[i][j]);
+        }
+        printf("\n");
+    }
 
 
     return 0;
